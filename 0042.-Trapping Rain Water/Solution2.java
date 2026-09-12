35b10ad6f0c011658d6378547d2679c4ecd42d6c
Nikhil Singh Chouhan <chouhannikhil203@gmail.com>
2026-09-13 00:41:52 +0530

    Create Solution2.java

---
+++ 0042.-Trapping Rain Water/Solution2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int trap(int[] height) {
+    int ans =0;
+    int left = 0;
+    int right = height.length-1;
+    int lmax =0;
+    int rmax =0;
+    while(left<right){
+        lmax = Math.max(lmax, height[left]);
+        rmax = Math.max(rmax, height[right]);
+
+        if(lmax < rmax){
+            ans += (lmax - height[left]);
+            left++;
+        }else{
+            ans += (rmax - height[right]);
+            right--;
+        }
+    }
+    return ans;
+    }
+}
